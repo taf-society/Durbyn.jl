8bd4bbfc5cd9470ad5d83106e97aeaba91dbaedc
Resul Akay <akay.86@hotmail.com>
2025-03-18 13:45:44 +0100

    arima from arima_transpar from R stat C lib

--- src/arima/arima_gradient_transform.jl
+++ src/arima/arima_gradient_transform.jl
@@ -5,11 +5,11 @@ function arima_param_transform!(p::Int, raw::AbstractVector, new::AbstractVector
 
     new[1:p] .= tanh.(raw[1:p])
     work = copy(new[1:p])
-    
-    for j in 2:p
+
+    for j = 2:p
         a = new[j]
-        for k in 1:(j-1)
-            work[k] -= a * new[j - k]
+        for k = 1:(j-1)
+            work[k] -= a * new[j-k]
         end
         new[1:(j-1)] .= work[1:(j-1)]
     end
@@ -20,23 +20,23 @@ function arima_gradient_transform(x::AbstractArray, arma::AbstractArray)
     mp, mq, msp = arma[1:3]
     n = length(x)
     y = Matrix{Float64}(I, n, n)
-    
+
     w1 = Vector{Float64}(undef, 100)
     w2 = Vector{Float64}(undef, 100)
     w3 = Vector{Float64}(undef, 100)
-    
+
     if mp > 0
-        
-        for i in 1:mp
+
+        for i = 1:mp
             w1[i] = x[i]
         end
-        
+
         arima_param_transform!(mp, w1, w2)
-        
-        for i in 1:mp
+
+        for i = 1:mp
             w1[i] += eps
             arima_param_transform!(mp, w1, w3)
-            for j in 1:mp
+            for j = 1:mp
                 y[i, j] = (w3[j] - w2[j]) / eps
             end
             w1[i] -= eps
@@ -45,15 +45,15 @@ function arima_gradient_transform(x::AbstractArray, arma::AbstractArray)
 
     if msp > 0
         v = mp + mq
-        for i in 1:msp
-            w1[i] = x[i + v]
+        for i = 1:msp
+            w1[i] = x[i+v]
         end
         arima_param_transform!(msp, w1, w2)
-        for i in 1:msp
+        for i = 1:msp
             w1[i] += eps
             arima_param_transform!(msp, w1, w3)
-            for j in 1:msp
-                y[i + v, j + v] = (w3[j] - w2[j]) / eps
+            for j = 1:msp
+                y[i+v, j+v] = (w3[j] - w2[j]) / eps
             end
             w1[i] -= eps
         end
@@ -72,4 +72,53 @@ function arima_undo_params(x::AbstractArray, arma::AbstractArray)
         arima_param_transform!(msp, @view(x[v+1:end]), @view(res[v+1:end]))
     end
     return res
+end
+
+function arima_transpar(
+    params_in::Vector{Float64},
+    arma::Vector{Int},
+    trans::Bool;
+    partrans = arima_param_transform!,
+)
+    mp, mq, msp, msq, ns = arma
+    p = mp + ns * msp
+    q = mq + ns * msq
+
+    phi = zeros(Float64, p)
+    theta = zeros(Float64, q)
+    params = copy(params_in)
+
+    if trans
+        if mp > 0
+            partrans(mp, params_in, params)
+        end
+        v = mp + mq
+        if msp > 0
+            partrans(msp, params_in[v+1:end], params[v+1:end])
+        end
+    end
+
+    if ns > 0
+        phi[1:mp] .= params[1:mp]
+        theta[1:mq] .= params[mp+1:mp+mq]
+
+        for j = 0:(msp-1)
+            phi[(j+1)*ns] += params[mp+mq+j+1]
+            for i = 0:(mp-1)
+                phi[((j+1)*ns)+(i+1)] -= params[i+1] * params[mp+mq+j+1]
+            end
+        end
+
+        for j = 0:(msq-1)
+            theta[(j+1)*ns] += params[mp+mq+msp+j+1]
+            for i = 0:(mq-1)
+                theta[((j+1)*ns)+(i+1)] += params[mp+i+1] * params[mp+mq+msp+j+1]
+            end
+        end
+    else
+        phi[1:mp] .= params[1:mp]
+        theta[1:mq] .= params[mp+1:mp+mq]
+    end
+
+    return (phi, theta)
 end

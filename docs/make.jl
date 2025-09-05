3e3561e6dfa5dc7bdbc3f9e326d3d51b17f15950
Resul Akay <akay.86@hotmail.com>
2025-09-05 08:54:21 +0200

    re-run docs

--- docs/make.jl
+++ docs/make.jl
@@ -12,23 +12,20 @@ makedocs(;
         canonical = "https://taf-society.github.io/Durbyn.jl/",
         assets = String[joinpath(@__DIR__, "src", "assets", "theme.css")],
         analytics = nothing,
-        footer = "<div class=\"footer\">Made with ❤ using Documenter.jl • © $(year(now())) Time Series Analysis and Forecasting Society</div>",
+        footer = "<div class=\"footer\">Made with ❤ using Documenter.jl • © 2025 Time Series Analysis and Forecasting Society</div>",
     ),
     pages = [
-        pages = [
-            "Home" => "index.md",
-            #"Quick Start" => "quickstart.md",
-            # "User Guide" => Any[
-            #     "Exponential Smoothing"=>"expsmoothing.md",
-            #     "Intermittent Demand"=>"intermittent.md",
-            #     "ARIMA"=>"arima.md",
-            #     "ARAR/ARARMA"=>"ararma.md",
-            # ],
-            #"API Reference" => "api.md",
+        "Home" => "index.md",
+        "Quick Start" => "quickstart.md",
+        "User Guide" => [
+            "Exponential Smoothing" => "expsmoothing.md",
+            "Intermittent Demand"   => "intermittent.md",
+            "ARIMA"                  => "arima.md",
+            "ARAR/ARARMA"            => "ararma.md",
         ],
+        "API Reference" => "api.md",
     ],
 )
-
 
 deploydocs(
     repo = "github.com/taf-society/Durbyn.jl",

05c008227246ced37e71b266c3931f8241256902
Resul Akay <akay.86@hotmail.com>
2025-10-01 23:44:31 +0200

    Fix case-insensitive filesystem conflict in Generics module Fixes #9

--- src/Generics/Generics.jl
+++ src/Generics/Generics.jl
@@ -4,7 +4,14 @@ using Plots
 import Base: show, summary
 include("forecast.jl")
 include("plot.jl")
-include("generics.jl")
+
+# Generic function definitions
+function fitted end
+function residuals end
+function predict end
+function coef end
+function coefficients end
+function coefs end
 
 export Forecast, forecast
 export plot, fitted, residuals, summary, predict
--- src/Generics/generics.jl
+++
@@ -1,6 +0,0 @@
-function fitted end
-function residuals end
-function predict end
-function coef end
-function coefficients end
-function coefs end

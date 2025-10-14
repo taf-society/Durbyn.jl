906a076560253f8ceab352c6fee0c062deb4cdae
Resul Akay <akay.86@hotmail.com>
2025-10-14 11:04:19 +0200

    new exports in optim

--- src/Optimize/Optimize.jl
+++ src/Optimize/Optimize.jl
@@ -1,11 +1,22 @@
 module Optimize
 
+include("numgrad.jl")
 include("nmmin.jl")
 include("bfgs.jl")
+include("lbfgsbmin.jl")
 include("fmin.jl")
 include("optim_hessian.jl")
 include("scalers.jl")
+include("optim.jl")
 
-export nmmin, bfgsmin, fmin, optim_hessian, NelderMeadOptions, BFGSOptions, FminOptions, scaler, descaler
+
+export nmmin, lbfgsbmin, fmin, optim_hessian
+export NelderMeadOptions, BFGSOptions, LBFGSBOptions, FminOptions
+export scaler, descaler
+export optim  # Unified R-like interface
+export numgrad, numgrad!, numgrad_with_cache!
+export NumericalGradientCache
+export bfgsmin, bfgs_hessian_update!
+export BFGSWorkspace
 
 end

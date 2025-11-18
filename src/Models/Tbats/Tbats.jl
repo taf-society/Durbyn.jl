ac2e256c3c5998a4545289295352845056a8b7cf
Resul Akay <akay.86@hotmail.com>
2025-11-18 14:48:20 +0100

    [Feat] tbats module

---
+++ src/Models/Tbats/Tbats.jl
@@ -0,0 +1,18 @@
+module Tbats
+
+import ..Utils: na_contiguous, is_constant
+import ..Stats: box_cox, inv_box_cox, box_cox_lambda
+import ..Arima: auto_arima
+import ..Generics: Forecast, forecast, fitted
+
+using LinearAlgebra: I, eigvals, dot
+using Statistics: mean
+using Distributions: Normal, quantile
+using Polynomials: Polynomial, roots
+using Optim: optimize, NelderMead, BFGS, Optim
+
+export tbats, TBATSModel
+
+include("tbats_base.jl")
+
+end
